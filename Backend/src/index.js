32b370b00f3801d0439c23797721326a5be82324
Naitik Patel <np03022005@gmail.com>
2024-10-04 09:53:04 +0530

    error debugging

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import admin from 'firebase-admin';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRoutes from './Routes/UserRoutes.js';
 
 dotenv.config({
@@ -15,6 +16,7 @@ app.use(cors({
   credentials: true
 }))
 
+app.use(cookieParser());
 app.use('/user', userRoutes); 
 
 app.listen(3000, () => {
--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -3,22 +3,24 @@ import admin from '../Utils/firebase.js';
 async function auth(req, res, next) {
   if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
       !req.cookies.__session) {
-    console.error('No Firebase ID token was passed in the Authorization header.');
+    console.error('No Firebase ID token was passed in the Authorization header or cookies.');
     return res.status(403).json({ message: 'Unauthorized' });
   }
 
   let idToken;
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
     idToken = req.headers.authorization.split('Bearer ')[1];
-  }
-  else{
-    console.log('No token in headers');
+  } else if (req.cookies.__session) {
+    idToken = req.cookies.__session;
+  } else {
+    console.log('No token in headers or cookies');
     return res.status(403).json({ message: 'Unauthorized' });
   }
+
   try {
     const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-    req.user = decodedIdToken;  
-    console.log('decodedIdToken', decodedIdToken,req.user.uid);
+    req.user = decodedIdToken;
+    console.log('decodedIdToken', decodedIdToken, req.user.uid);
     next();
   } catch (error) {
     console.error('Error while verifying Firebase ID token:', error);
